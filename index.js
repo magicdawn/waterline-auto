84ebf8e8d1b1ad76ba13874e72b55480e0bfa6ec
magicdawn@qq.com <magicdawn@qq.com>
2016-07-01 18:56:29 +0800

    wa: add

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 'use strict';
 
+/**
+ * Module dependencies
+ */
+
 const SequelizeAuto = require('sequelize-auto');
 require('promise.ify').all(SequelizeAuto.prototype);
 const co = require('co');
+const _ = require('lodash');
 
-const main = co.wrap(function*() {
-  const auto = new SequelizeAuto('stock', 'admin', 'nopass.0601', {
-    tables: ['TB_TRADE_ACCOUNT'],
-    host: '10.255.52.118'
+/**
+ * getTables
+ */
+
+const getTables = exports.getTables = co.wrap(function*(options) {
+  const host = options.host || 'localhost';
+  const port = options.port || '3306';
+  const database = options.database;
+  const user = options.user;
+  const password = options.password;
+  const tables = options.tables;
+
+  const auto = new SequelizeAuto(database, user, password, {
+    host: host,
+    port: port,
+    tables: tables
   });
 
   yield auto.buildAsync();
-  console.log(auto.tables);
+  return auto.tables;
 });
 
-main().catch(console.error);
+/**
+ * transform a table
+ */
+
+const transform = function(name, table) {
+  const ret = {
+    tableName: name,
+    attributes: {}
+  };
+
+  _.each(table, (v, field) => {
+    ret.attributes[field] = {};
+    // const
+
+  });
+};
