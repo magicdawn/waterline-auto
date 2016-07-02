6b29a4c7c21add3844ab6b323f1df9b99703c0ab
magicdawn@qq.com <magicdawn@qq.com>
2016-07-02 10:05:38 +0800

    wa: 基本可用

--- index.js
+++ index.js
@@ -35,15 +35,73 @@ const getTables = exports.getTables = co.wrap(function*(options) {
  * transform a table
  */
 
-const transform = function(name, table) {
+const transform = exports.transform = function(tableName, table) {
   const ret = {
-    tableName: name,
-    attributes: {}
+    tableName: tableName
   };
 
-  _.each(table, (v, field) => {
-    ret.attributes[field] = {};
-    // const
-
+  // autoUpdatedAt / autoCreatedAt
+  ['updatedAt', 'createdAt'].forEach(k => {
+    if (!table[k]) {
+      const key = 'auto' + _.upperFirst(k);
+      ret[key] = false;
+    }
   });
+
+  // attributes
+  ret.attributes = {};
+  _.each(table, (fieldDef, fieldName) => {
+    const key = _.camelCase(fieldName);
+    const o = ret.attributes[key] = {};
+
+    // columnName
+    if (key !== fieldName) o.columnName = fieldName;
+
+    // type
+    o.type = getType(fieldDef.type);
+
+    // primaryKey
+    if (fieldDef.primaryKey) o.primaryKey = true;
+
+    // defaults
+    if (fieldDef.defaultValue) o.defaultsTo = fieldDef.defaultValue;
+  });
+
+  return ret;
+};
+
+const getType = t => {
+  const _attr = t.toLowerCase();
+  let val;
+
+  if (_attr === 'tinyint(1)' || _attr === 'boolean' || _attr === 'bit(1)') {
+    val = 'boolean';
+  } else if (_attr.match(/^(smallint|mediumint|tinyint|int)/)) {
+    val = 'integer';
+  } else if (_attr.match(/^bigint/)) {
+    val = 'integer';
+  } else if (_attr.match(/^string|varchar|varying|nvarchar/)) {
+    val = 'STRING';
+  } else if (_attr.match(/^char/)) {
+    val = 'string';
+  } else if (_attr.match(/text|ntext$/)) {
+    val = 'text';
+  } else if (_attr.match(/^(date)/)) {
+    val = 'date';
+  } else if (_attr.match(/^(time)/)) {
+    val = '<uknown type>';
+  } else if (_attr.match(/^(float|float4)/)) {
+    val = 'FLOAT';
+  } else if (_attr.match(/^decimal/)) {
+    val = 'float';
+  } else if (_attr.match(/^json/)) {
+    val = 'json';
+  } else if (_attr.match(/^jsonb/)) {
+    val = 'JSONB';
+  } else if (_attr.match(/^geometry/)) {
+    val = 'GEOMETRY';
+  } else {
+    val = '<uknown type>';
+  }
+  return val && val.toLowerCase();
 };
