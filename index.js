554ffef76185735d942ab542bb37cace999e9864
magicdawn@qq.com <magicdawn@qq.com>
2016-07-04 21:26:20 +0800

    wa: add comment/sequelize-log support

--- index.js
+++ index.js
@@ -6,38 +6,45 @@
 
 const SequelizeAuto = require('sequelize-auto');
 require('promise.ify').all(SequelizeAuto.prototype);
-const co = require('co');
+const async = require('co').wrap;
 const _ = require('lodash');
 
 /**
  * getTables
  */
 
-const getTables = exports.getTables = co.wrap(function*(options) {
+const getTables = exports.getTables = async(function*(options) {
   const host = options.host || 'localhost';
   const port = options.port || '3306';
   const database = options.database;
   const user = options.user;
   const password = options.password;
   const tables = options.tables;
-
+  const sequelizeLog = Boolean(options.sequelizeLog);
+  
   const auto = new SequelizeAuto(database, user, password, {
     host: host,
     port: port,
-    tables: tables
+    tables: tables,
+    logging: sequelizeLog
   });
 
   yield auto.buildAsync();
-  return auto.tables;
+  return auto;
 });
 
 /**
  * transform a table
  */
 
-const transform = exports.transform = function(tableName, table, options) {
-  options = options || {};
+const transform = exports.transform = async(function*(options) {
+  if (!options) throw new Error('table & tableName & dbName cann\'t be empty');
   const rawType = Boolean(options.rawType);
+  const comment = Boolean(options.comment);
+  const seqAuto = options.seqAuto;
+  const dbName = options.dbName;
+  const tableName = options.tableName;
+  const table = options.table;
   const ret = {
     tableName: tableName
   };
@@ -52,26 +59,37 @@ const transform = exports.transform = function(tableName, table, options) {
 
   // attributes
   ret.attributes = {};
-  _.each(table, (fieldDef, fieldName) => {
-    const key = _.camelCase(fieldName);
+  for (let columnName in table) {
+    const column = table[columnName];
+    const key = _.camelCase(columnName);
     const o = ret.attributes[key] = {};
 
     // columnName
-    if (key !== fieldName) o.columnName = fieldName;
+    if (key !== columnName) o.columnName = columnName;
 
     // type
-    o.type = getType(fieldDef.type).toLowerCase();
-    if (rawType) o.rawType = fieldDef.type;
+    o.type = getType(column.type).toLowerCase();
+    if (rawType) o.rawType = column.type;
 
     // primaryKey
-    if (fieldDef.primaryKey) o.primaryKey = true;
+    if (column.primaryKey) o.primaryKey = true;
 
     // defaults
-    if (fieldDef.defaultValue) o.defaultsTo = fieldDef.defaultValue;
-  });
+    if (column.defaultValue) o.defaultsTo = column.defaultValue;
+
+    // comment
+    if (options.comment) {
+      o.comment = yield getComment({
+        seq: seqAuto.sequelize,
+        dbName: dbName,
+        tableName: tableName,
+        columnName: columnName
+      });
+    }
+  }
 
   return ret;
-};
+});
 
 // http://sailsjs.org/documentation/concepts/models-and-orm/attributes
 const getType = t => {
@@ -105,3 +123,28 @@ const getType = t => {
 
   return '<uknown type>';
 };
+
+const getComment = exports.getComment = async(function*(options) {
+  options = options || {};
+  const seq = options.seq;
+  const dbName = options.dbName;
+  const tableName = options.tableName;
+  const columnName = options.columnName;
+
+  const sql = `
+  SELECT COLUMN_COMMENT as comment
+  FROM information_schema.COLUMNS
+  WHERE TABLE_SCHEMA=:dbName
+    && TABLE_NAME=:tableName
+    && COLUMN_NAME=:columnName
+  `;
+  const result = yield seq.query(sql, {
+    replacements: {
+      dbName: dbName,
+      tableName: tableName,
+      columnName: columnName
+    }
+  });
+
+  return result[0] && result[0][0] && result[0][0].comment;
+});
